64dbb054ae30163e0ef8e9f6deb8fb8142abcf1a
Guangwei Tang <g24tang@uwaterloo.ca>
2025-03-08 14:36:19 +0800

    updated getTokenFromWeb to generate refresh token

--- main.go
+++ main.go
@@ -74,26 +74,8 @@ func setupGmailService() {
 	}
 }
 
-// func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-// 	//state-token will be replaced with a dynamic token later on
-// 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-// 	//hardcode authorization for now
-// 	fmt.Printf("Go to this link and authorize: %v\nThen paste the code here: ", authURL)
-
-//		var authCode string
-//		if _, err := fmt.Scan(&authCode); err != nil {
-//			log.Fatalf("Unable  to read authorization code: %v", err)
-//		}
-//		fmt.Println("Authorization code received.")
-//		tok, err := config.Exchange(context.TODO(), authCode)
-//		fmt.Println("Token received.")
-//		if err != nil {
-//			log.Fatalf("Unable to acquire token: %v", err)
-//		}
-//		return tok
-//	}
 func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline, oauth2.SetAuthURLParam("prompt", "consent"))
 	fmt.Printf("Go to this link and authorize: %v\nThen paste the code here: ", authURL)
 
 	var authCode string
@@ -158,13 +140,49 @@ func saveToken(path string, token *oauth2.Token) {
 		log.Fatalf("Unable to encodetoken: %v", err)
 	}
 }
+
+func listEmails(numofemails int) ([]*gmail.Message, error) {
+	user := "me"
+	r, err := srv.Users.Messages.List(user).MaxResults(int64(numofemails)).Do()
+	if err != nil {
+		return nil, err
+	}
+	return r.Messages, nil
+}
+
+func getEmails(emailid string) (*gmail.Message, error) {
+	user := "me"
+	msg, err := srv.Users.Messages.Get(user, emailid).Do()
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve emails details: %v", err)
+	}
+	return msg, nil
+}
+
+//	func decodeEmail(msg *gmail.Message) (string, error) {
+//		for _, part := range msg.Payload.Parts {
+//			if part.MimeType == "text/plain" || part.MimeType == "text/html" {
+//				data, _ := base64.URLEncoding.DecodeString(part.Body.Data)
+//				return string(data)
+//			}
+//		}
+//		return ""
+//	}
 func main() {
 	r := gin.Default()
 	setupGmailService()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	r.GET("/emails", func(c *gin.Context) {
+		emails, err := listEmails(10)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, emails)
 	})
 	fmt.Println("Server started on http://localhost:8080")
 	r.Run(":8080")
